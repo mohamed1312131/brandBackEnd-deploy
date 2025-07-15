718e493f4cc8f8604e3bec93e484e31200442421
mohamed1312131 <mohamed1312131@gmail.com>
2025-07-15 12:01:13 +0100

    correct health status

--- src/main/java/com/example/hamzabackend/controller/HealthController.java
+++ src/main/java/com/example/hamzabackend/controller/HealthController.java
@@ -1,14 +1,14 @@
-package com.example.brandBackEnd.controller;
+package com.example.hamzabackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/health")
 public class HealthController {
     
-    @GetMapping("/health")
+    @GetMapping
     public String health() {
         return "OK";
     }
