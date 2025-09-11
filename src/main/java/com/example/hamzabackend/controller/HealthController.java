19ff82904306a3f5d58a0a2f552339541a1f0c5b
mohamed1312131 <mohamed1312131@gmail.com>
2025-09-11 16:03:37 +0100

    test deploy

--- src/main/java/com/example/hamzabackend/controller/HealthController.java
+++ src/main/java/com/example/hamzabackend/controller/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/health")
 public class HealthController {
-    
+    // this comment
     @GetMapping
     public String health() {
         return "OK";
